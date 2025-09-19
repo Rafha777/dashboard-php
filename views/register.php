05ca57322a4fc42225984d56b38262e57c4c47ca
Rafha777 <rafhaellalvesvieira@gmail.com>
2025-09-19 16:23:52 -0300

    Create register.php

---
+++ views/register.php
@@ -0,0 +1,8 @@
+<h2>Registrar</h2>
+<form method="POST">
+    <input type="text" name="name" placeholder="Nome" required><br>
+    <input type="email" name="email" placeholder="Email" required><br>
+    <input type="password" name="password" placeholder="Senha" required><br>
+    <button type="submit">Registrar</button>
+</form>
+<a href="index.php?page=login">Login</a>
