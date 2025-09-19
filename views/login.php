2a3ad61216b581830ee0e5822e632148fac3f0f1
Rafha777 <rafhaellalvesvieira@gmail.com>
2025-09-19 16:23:31 -0300

    Create login.php

---
+++ views/login.php
@@ -0,0 +1,7 @@
+<h2>Login</h2>
+<form method="POST">
+    <input type="email" name="email" placeholder="Email" required><br>
+    <input type="password" name="password" placeholder="Senha" required><br>
+    <button type="submit">Login</button>
+</form>
+<a href="index.php?page=register">Cadastrar</a>
