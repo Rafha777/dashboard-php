3502fd1477131ca33672db9b8879cd0ce8f215e6
Rafha777 <rafhaellalvesvieira@gmail.com>
2025-09-19 16:25:27 -0300

    Create task_add.php

---
+++ views/task_add.php
@@ -0,0 +1,7 @@
+<h2>Adicionar Tarefa</h2>
+<form method="POST">
+    <input type="text" name="title" placeholder="Título" required><br>
+    <textarea name="description" placeholder="Descrição"></textarea><br>
+    <button type="submit">Salvar</button>
+</form>
+<a href="index.php?page=dashboard">Voltar</a>
